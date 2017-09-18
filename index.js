502de4fa76a00890ba1abc8c76a0451b554be6c8
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-18 10:11:32 +0200

    adde views

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express')
 var app = express()
 
 
-app.set('port', process.env.PORT || 3000);
+app.disable('x-powered-by')
+
+
+
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', function(req, res){
   res.send('Express Works!')
